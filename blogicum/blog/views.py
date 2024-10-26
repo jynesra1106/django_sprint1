6fbbf6976a492c6782d48d520e41027178aa507d
Арсений Пирогов <pirogovarsenij403@gmail.com>
2024-10-26 18:48:32 +0500

    beb8

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+
 
 posts = [
     {
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def about(request):
     return render(request, 'html/about.html')
+
 
 def rules(request):
     return render(request, 'html/rules.html')
